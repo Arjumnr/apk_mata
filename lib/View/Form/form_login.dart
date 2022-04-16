1bdd29a8c9df8fb90e2d351ce69ff7b498a0ff8b
Arjumnr <arjumnurramadhan@gmail.com>
2022-04-17 02:42:07 +0800

    login

--- lib/View/Form/form_login.dart
+++ lib/View/Form/form_login.dart
@@ -6,6 +6,9 @@ class FormLogin extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //
+    Size size = MediaQuery.of(context).size;
+    //
     final logo = Hero(
       tag: 'hero',
       child: CircleAvatar(
@@ -14,14 +17,50 @@ class FormLogin extends StatelessWidget {
         child: Image.asset('assets/images/logo.png'),
       ),
     );
+    //
+    final email = Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          border: Border(bottom: BorderSide(color: Colors.grey.shade200))),
+      child: Card(
+        child: TextField(
+          decoration: InputDecoration(
+              hintText: "Username",
+              hintStyle: TextStyle(color: Colors.grey),
+              border: InputBorder.none),
+        ),
+      ),
+    );
+    //
+    final password = TextFormField(
+      autofocus: false,
+      initialValue: '',
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+    //
+    final loginButton = Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        onPressed: () {},
+        padding: EdgeInsets.all(12),
+        color: Colors.lightBlueAccent,
+        child: Text('Log In', style: TextStyle(color: Colors.white)),
+      ),
+    );
     return Scaffold(
       backgroundColor: background,
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            logo,
-          ],
+          children: [],
         ),
       ),
     );
